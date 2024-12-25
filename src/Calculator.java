5907177f584a9fad41203fed669eba57a4f90a4a
wizkid1977 <wizkid1977@gmail.com>
2024-12-25 11:46:54 +0200

    Add files via upload

---
+++ src/Calculator.java
@@ -0,0 +1,30 @@
+public class Calculator {
+
+    /**
+     * This method adds two numbers and prints the result
+     * @param a first number
+     * @param b second number
+     */
+    public static void addNumbers(int a, int b) {
+        System.out.println("Sum: " + (a+b));
+    }
+
+
+    /**
+     * This method adds all adjacent numbers in a given array
+     * @param numbers the array of numbers
+     */
+    public static void addLotsOfNumbers(int[] numbers){
+        for(int i=0; i<numbers.length; i++){
+            addNumbers(numbers[i], numbers[i+1]);
+        }
+    }
+
+    public static void divideNumbers(int a, int b) {
+        System.out.println("Division: " + (a/b));
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
