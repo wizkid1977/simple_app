dec4dafb6260d3b288f8cefb496972a89eef99b0
Johnbryce Trainer <jbt@johnbryce.co.il>
2024-12-25 12:15:24 +0200

    more updates on Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,18 +8,16 @@ public class Calculator {
     public static void addNumbers(int a, int b) {
         int sum = a + b;
         System.out.println("Sum: " + sum);
-
     }
 
 
     /**
-     * This method adds all adjacent numbers in a given array
-     * @param numbers the array of numbers
+     * Bla bla bla
+     * @param numbers is the numbers array
      */
     public static void addLotsOfNumbers(int[] numbers){
         for(int i=0; i<numbers.length; i++){
             addNumbers(numbers[i], numbers[i+1]);
-            System.out.println("bla");
         }
     }
 
